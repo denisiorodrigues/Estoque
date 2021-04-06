1d67b327f0150c8c88a20989bdf5e80ba135a018
denisiodrigues <denisio@ymail.com>
2021-04-06 09:41:45 -0300

    Customização das Views- Somente Layout

--- src/DR.App/ViewsModels/ProdutoViewModel.cs
+++ src/DR.App/ViewsModels/ProdutoViewModel.cs
@@ -24,6 +24,7 @@ namespace DR.App.ViewsModels
         [StringLength(1000, ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
         
+        [DisplayName("Imagem do Produto")]
         public IFormFile ImagemUpload { get; set; }
 
         public string Imagem { get; set; }
