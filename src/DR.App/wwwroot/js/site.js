a053a7e70a3237a57a2bc9ed59eff72c86cc22cc
denisiodrigues <denisio@ymail.com>
2021-04-14 07:56:31 -0300

    Utilizando Ajax para preenchimento do CEP

--- src/DR.App/wwwroot/js/site.js
+++ src/DR.App/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿function AjaxModal() {
+﻿function AjaxModal()
+{
     $(document).ready(function () {
         $(function () {
             $.ajaxSetup({ cache: false });
@@ -34,5 +35,57 @@
                 return false;
             });
         }
+    });
+}
+
+function BuscaCEP() {
+    $(document).ready(function () {
+        function limpa_fomulario_cep() {
+            //Limmpa os fomrulários do cep
+            $("#Endereco_Logradouro").val('');
+            $("#Endereco_Bairro").val('');
+            $("#Endereco_Cidade").val('');
+            $("#Endereco_Estado").val('');
+        }
+
+        //Quando o campo cep muda o foco
+        $("#Endereco_Cep").blur(function () {
+            //Nova variável cep somente com dígitos
+            var cep = $(this).val().replace(/\D/g, '');
+
+            //Verifica se o campo cep tem valor informado
+            if (cep != "") {
+                //Expressão regular para validar o cep
+                var validaCep = /^[0-9]{8}$/;
+
+                //valida o formato do ceo
+                if (validaCep.test(cep)) {
+                    //Preenche os campos com "..." (reticencias) enquanto consulta o web service
+                    $("#Endereco_Logradouro").val("...");
+                    $("#Endereco_Bairro").val("...");
+                    $("#Endereco_Cidade").val("...");
+                    $("#Endereco_Estado").val("...");
+
+                    //Consulta o webservice viacep.com.br
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+                            if (!("erro" in dados)) {
+                                //Atualiza os campso com os valores da consulta.
+                                $("#Endereco_Logradouro").val(dados.logradouro);
+                                $("#Endereco_Bairro").val(dados.bairro);
+                                $("#Endereco_Cidade").val(dados.localidade);
+                                $("#Endereco_Estado").val(dados.uf);
+                            } else {
+                                //CEP pesquisado não foi encontrado
+                                limpa_fomulario_cep();
+                                alert("Formato de CEP inválido");
+                            }
+                        });
+                } else {
+                    //cep sem valor, limpa o formulário.
+                    limpa_fomulario_cep();
+                }
+            }
+        });
     });
 }
