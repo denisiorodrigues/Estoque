9e50241a0a389cb370943909d4bd37a5c2d41df7
denisiodrigues <denisio@ymail.com>
2021-04-18 12:05:32 -0300

    Organizando e otimizando a estrutura da App

---
+++ src/DR.App/Configurations/DependecyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using DR.App.Extensions;
+using DR.Business.Interfaces;
+using DR.Data.Context;
+using DR.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DR.App.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependences(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+}
